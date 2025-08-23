bf445ca41dd53fa9af14f315e5c95bf0bfa36b6c
Mohammad-Kholghi <mohammad.kholqi@gmail.com>
2025-08-23 20:45:22 +0330

    looks like init.js is OK

--- commands/init.js
+++ commands/init.js
@@ -4,7 +4,6 @@ const homedir = require('os').homedir();
 const yaml = require('yaml');
 const { sshExec } = require('../lib/exec');
 const ArvanProvider = require('../lib/arvan');
-const Cmd = require('../lib/Commands');
 
 
 exports.command = ['init'];
@@ -30,61 +29,61 @@ exports.handler = async argv => {
 	// hint: you should store an inventory file when you create the instance. 
 	//       that way your ansible roles can use the inventory file to run against the instance you just created
 
-	let p = new ArvanProvider({ token: process.env.ARVAN_TOKEN });
+	// USE LOCAL SERVER?
+	let useLocal = process.env.USE_LOCAL === "true";
 	
-	console.log('Provisioning...');
+	// SERVER INFO
+	let sshServer, sshUser;
+	let sshKey = path.join(homedir, process.env.SSH_KEY);
 	
-	const result = await p.create({
-		'name': "testserver",
-		'region': 'ir-thr-ba1',
-		'size': 'eco-1-1-0',
-		'image': '793cdb82-c731-4aeb-808d-19da98525a02'
-	});
-
-	// TODO 6: next wait for the VM to be ready and then get and print its IP address
-	console.log(`Server ID: ${result.data.id} , IP Address: ${result.data.security_groups[0].ip_addresses[0]}`);
-	console.log(`${result.data.password}`);
-
-	console.log('Done!');
-	
-	
-	
-	let server = result.data.security_groups[0].ip_addresses[0];
-	
-	// return;
-	
+	if(useLocal){
+		sshServer = process.env.LOCAL_SERVER;
+		sshUser = process.env.LOCAL_USER;
+	} else {
+		
+		const p = new ArvanProvider({ token: process.env.ARVAN_TOKEN });
+		console.log('Provisioning...');
+		
+		const result = await p.create({
+			'name': process.env.SERVER_NAME,
+			'region': process.env.SERVER_REGION,
+			'size': process.env.SERVER_SIZE,
+			'image': process.env.SERVER_IMAGE
+		});
+		
+		// TODO 6: next wait for the VM to be ready and then get and print its IP address
+		console.log(`Server ID: ${result.data.id} , IP Address: ${result.data.security_groups[0].ip_addresses[0]}`);
+		console.log(`${result.data.password}`);
+		
+		console.log('Done!');
+		
+		sshUser = 'ubuntu';
+		sshServer = result.data.security_groups[0].ip_addresses[0];
+	}
 	
 	const buildYamlContent = await fs.promises.readFile(build, 'utf8');
 	const buildYamlContentParsed = yaml.parse(buildYamlContent);
-	// console.log(buildYamlContentParsed);
-	
-	let cmd = new Cmd(server, 'ubuntu', path.join(homedir, '/.ssh/id_rsa'));
-	
-	
 
 	for (const setup of buildYamlContentParsed.setup) {
-		console.log(setup);
+		
+		console.log(`setup is ${setup}`);
+		
 		if (setup.command) {
 			// TODO: run the command on the instance
-			//???????????????????????????
-			await cmd.git(setup.cpmmand).then(() => {
-				console.log(`installed apt ${setup.command}`)
+			await sshExec(sshUser, sshServer, sshKey, `${setup.command}`).then(() => {
+				console.log(`${setup.command} done`);
 			});
-			
-			// await sshExec('ubuntu', '37.32.9.61', path.join(homedir, '/.ssh/id_rsa'), `${setup.command}`).then(() => {
-			// 	console.log('done');
-			// });
 		}
 		else if (setup.apt) {
 			// TODO: run the apt command on the instance to install the packages
-			await sshExec('ubuntu', '37.32.9.61', path.join(homedir, '/.ssh/id_rsa'), `sudo apt install -y ${setup.apt}`).then(() => {
-				console.log('done');
+			await sshExec(sshUser, sshServer, sshKey, `sudo apt install -y ${setup.apt}`).then(() => {
+				console.log(`Installing ${setup.apt} done`);
 			});
 		}
 		else if (setup.git) {
 			// TODO: clone the git repo on the instance
-			await sshExec('ubuntu', '37.32.9.61', path.join(homedir, '/.ssh/id_rsa'), `git clone ${setup.git}`).then(() => {
-				console.log('done');
+			await sshExec(sshUser, sshServer, sshKey, `git clone ${setup.git}`).then(() => {
+				console.log(`cloning ${setup.git} done`);
 			});
 		}
 		else if (setup.playbook) {
@@ -94,11 +93,4 @@ exports.handler = async argv => {
 			console.error('unknown setup command');
 		}
 	}
-
-
-	// run a command over ssh:
-	// sshExec('USERNAME', 'IP_ADDRESS', path.join(homedir, '/.ssh/id_rsa'), 'ls -al').then(() => {
-	//     console.log('done');
-	// });
-
 };
